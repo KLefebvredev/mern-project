6bfa93f2d8b5f59c1b8ee231cb480264e656555c
klefebvre <klefebvre.dev@gmail.com>
2021-08-24 14:40:48 +0200

    first commit bdd auth/register

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const userRoutes = require("./routes/user.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+const {checkUser, requireAuth} = require('./middleware/auth.middleware');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//jwt
+app.get('*', checkUser);
+app.get('/jwtid',requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id)
+});
+
+//route
+app.use('/api/user', userRoutes);
+
+//server
+app.listen(process.env.PORT, () => {
+  console.log(`Listenig on port ${process.env.PORT}`);
+});
