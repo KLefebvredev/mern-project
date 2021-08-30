ac5c407113777eab0179e4fcb7559fa5a250fb45
klefebvre <klefebvre.dev@gmail.com>
2021-08-30 17:36:01 +0200

    Page login et ajout redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { UidContext } from "./components/AppContext";
 import Routes from "./components/Routes";
+import { UidContext } from "./components/AppContext";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getUser } from "./actions/user.actions";
 
 const App = () => {
   const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -15,10 +18,12 @@ const App = () => {
       })
         .then((res) => {
           setUid(res.data);
-    })
+        })
         .catch((err) => console.log("No token"));
     };
     fetchToken();
+
+    if (uid) dispatch(getUser(uid));
   }, [uid]);
 
   return (
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { UidContext } from "./AppContext";
+import Logout from "./Log/Logout";
+
+const Navbar = () => {
+  const uid = useContext(UidContext);
+  const userData = useSelector((state) => state.userReducer);
+
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="logo">
+          <NavLink exact to="/">
+            <div className="logo">
+              <img src="./img/icon.png" alt="Icon du site Raccoont" />
+              <h3>Raccoont</h3>
+            </div>
+          </NavLink>
+        </div>
+
+        {uid ? (
+          <ul>
+            <li></li>
+            <li className="img-navbar">
+              <NavLink exact to="/profil">
+                <img src={userData.picture} alt="Photo de profil de"/>
+              </NavLink>
+            </li>
+            <li className="welcome">
+              <NavLink exact to="/profil">
+                <h5>Bienvenue {userData.pseudo}</h5>
+              </NavLink>
+            </li>
+            <Logout />
+          </ul>
+        ) : (
+          <ul>
+            <li></li>
+            <li>
+              <NavLink exact to="/profil">
+                <img
+                  src="./img/icons/login.svg"
+                  alt="Logo permettant de s'enregistrer"
+                />
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
